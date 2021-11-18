50343e133f48ccb37f3ca7209d7edc004ecb0ef6
daveysea <seankdavey@seznam.cz>
2021-11-18 16:29:40 +0100

    More updates to frontend - seperated the "__main__" function to its own module   - will be better if you want to introduce logging or any kind of preprocessing later - added new setup and wishlist routes   - menu items added   - new routes and templates added   - url_for links added - added bike selector to each section page   - when bike is chosen from dropdown, the page reloads with the selected bike object passed to the page - cleaned up the "crumbs" and "bike_selector" jinja blocks   - now just minimal lines are needed add to each template page   - added one TODO to complete later - added placeholder charts into usage page  - can now work with them as python objects and have them as a grid-flexbox  - see "usage" route for current usage how to get data into charts

---
+++ main.py
@@ -0,0 +1,4 @@
+from web_gui import bike_garage
+
+if __name__ == "__main__":
+    bike_garage.run(debug=True)
--- web_gui.py
+++ web_gui.py
@@ -5,19 +5,16 @@ bike_garage = Flask(__name__)
 
 @bike_garage.route("/")
 @bike_garage.route("/index.html")
-def index():
-    return render_template("index.html")
+def index(bike_num=None):
+    return render_template("index.html",
+                           bike_data=bike_num)
 
 
-@bike_garage.route('/index.html/<int:bike_num>')
-def bike(bike_num=1):
-    #  ... read the bike data here based on the bike passed into the route from "bike selector" dropdown ...
-    return render_template('index.html', bike_data=bike_num)
-
-
-
-@bike_garage.route("/usage")
-def usage():
+@bike_garage.route("/usage/")
+def usage(bike_num=None):
+    chart_data = [10, 41, 35, 51, 49, 62, 69, 2, 148]
+    chart_data2 = [10, 41, 52, 1, 49, 62, 69, 91, 148]
+    chart_xseries = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
     ride_data = {
         "Date": "%d. %m. %Y",
         "Ride Type": "casual/high intensity/bikepark/jumps",
@@ -26,14 +23,25 @@ def usage():
         "Elevation Gain": "XXXkm/ft",
         "Strava link": "link in form of map (provided by strava)"
     }
-    return render_template("usage.html",
-                           ride_data=ride_data)
+    return render_template(
+        "usage.html",
+        chart_data=chart_data,
+        chart_data2=chart_data2,
+        chart_xseries=chart_xseries,
+        ride_data=ride_data
+    )
 
 
-@bike_garage.route("/components")
+@bike_garage.route("/components/")
 def components():
     return render_template("components.html")
 
 
-if __name__ == "__main__":
-    bike_garage.run(debug=True)
+@bike_garage.route("/setup/")
+def setup():
+    return render_template("setup.html")
+
+
+@bike_garage.route("/wishlist/")
+def wishlist():
+    return render_template("wishlist.html")
