db43af5c8b4b7bd645e710f1dfc89f4753dcb3c3
daveysea <seankdavey@seznam.cz>
2021-11-18 12:54:57 +0100

    Basically complete rewrite of the frontend :)
    - removed old bootstrap template, not easy to work with
    - used new "niceadmin" template, comes with lots of boilerplate files, can cut them down later
    - some examples of how to work correctly with jinja templating
       - importing from a base file
       - some basic for loops and if blocks
    - added new logic for "bikeselector" dropdown and how that will redirect the user to the correct page (new route -> /bike)
    - have a look around.
    
    - didnt add any UT yet, forgive me :/ I will get to it later I promise

--- web_gui.py
+++ web_gui.py
@@ -4,13 +4,30 @@ bike_garage = Flask(__name__)
 
 
 @bike_garage.route("/")
-def home():
-    return render_template("usage.html")
+@bike_garage.route("/index.html")
+def index():
+    return render_template("index.html")
+
+
+@bike_garage.route('/index.html/<int:bike_num>')
+def bike(bike_num=1):
+    #  ... read the bike data here based on the bike passed into the route from "bike selector" dropdown ...
+    return render_template('index.html', bike_data=bike_num)
+
 
 
 @bike_garage.route("/usage")
 def usage():
-    return render_template("usage.html")
+    ride_data = {
+        "Date": "%d. %m. %Y",
+        "Ride Type": "casual/high intensity/bikepark/jumps",
+        "Distance": "XXXkm/mil",
+        "Duration": "%H:%M",
+        "Elevation Gain": "XXXkm/ft",
+        "Strava link": "link in form of map (provided by strava)"
+    }
+    return render_template("usage.html",
+                           ride_data=ride_data)
 
 
 @bike_garage.route("/components")
